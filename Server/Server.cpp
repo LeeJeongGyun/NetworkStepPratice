42b1db524ff014676543bf57c4b01061ae6c2eaa
JKLEE <jeonggyun0630@gmail.com>
2024-02-09 12:01:27 +0900

    Socket Option

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -25,40 +25,46 @@ int main()
         return 0;
     }
 
-    SOCKADDR_IN sockAdr;
-    ::memset(&sockAdr, 0, sizeof(sockAdr));
-    sockAdr.sin_family = AF_INET;
-    sockAdr.sin_addr.s_addr = ::htonl(INADDR_ANY);
-    sockAdr.sin_port = ::htons(9999);
+    // SO_KEEPALIVE : 주기적으로 연결 상태 확인하여 끊어짐 감지(TCP)
+    bool enable = true;
+    ::setsockopt(servSock, SOL_SOCKET, SO_KEEPALIVE, reinterpret_cast<char*>(&enable), sizeof(enable));
 
-    if (SOCKET_ERROR == ::bind(servSock, reinterpret_cast<const SOCKADDR*>(&sockAdr), sizeof(sockAdr)))
+    // SO_LINER 
+    // onoff = 1 linger 옵션 활성화, 0 = default
+    // RST 보낼 때 사용
+    // 상대방과 4way handshaking 하지 않겠다. ( onoff = 1, linger = 0 )
+    LINGER linger;
+    linger.l_onoff = 1;
+    linger.l_linger = 0;
+    ::setsockopt(servSock, SOL_SOCKET, SO_LINGER, reinterpret_cast<char*>(&linger), sizeof(linger));
+
+    // SO_SNDBUF = 송신 버퍼 크기
+    // SO_RCVBUF = 수신 버퍼 크기
+    int sndBufSize;
+    int optionLen = sizeof(sndBufSize);
+    ::getsockopt(servSock,SOL_SOCKET, SO_SNDBUF, reinterpret_cast<char*>(&sndBufSize),&optionLen);
+    std::cout << "송신 버퍼 크기: " << sndBufSize << std::endl;
+
+    int rcvdBufSize;
+    optionLen = sizeof(rcvdBufSize);
+    ::getsockopt(servSock, SOL_SOCKET, SO_SNDBUF, reinterpret_cast<char*>(&rcvdBufSize), &optionLen);
+    std::cout << "수신 버퍼 크기: " << rcvdBufSize << std::endl;
+
+    // SO_REUSEADDR
+    // IP 주소 및 PORT 재사용
+    ::setsockopt(servSock, SOL_SOCKET, SO_REUSEADDR, reinterpret_cast<char*>(&enable), sizeof(enable));
+    
+    // IPPROTO_TCP
+    // TCP_NODELAY = Nagle 알고리즘
+    // 장점: 작은 패킷 불필요하게 많이 생성 x
+    // 단점: 반응 시간 손해
     {
-        HandleError("::bind error");
-        return 0;
+        bool enable = true;
+        ::setsockopt(servSock, IPPROTO_TCP, TCP_NODELAY, reinterpret_cast<char*>(&enable), sizeof(enable));
     }
 
-    SOCKADDR_IN clntAdr;
-    int clntAdrSize = sizeof(clntAdr);
-    while (true)
-    {
-        char recvBuffer[1000];
-        int recvSize = ::recvfrom(servSock, recvBuffer, sizeof(recvBuffer), 0, reinterpret_cast<SOCKADDR*>(&clntAdr), &clntAdrSize);
-        if (recvSize <= 0)
-        {
-            HandleError("recvfrom return <= 0");
-            return 0;
-        }
 
-        std::cout << "Recv Size: " << recvSize << std::endl;
-        std::cout << "Recv Data: " << recvBuffer << std::endl;
-        int sendSize = ::sendto(servSock, recvBuffer, recvSize, 0, reinterpret_cast<SOCKADDR*>(&clntAdr), sizeof(SOCKADDR_IN));
-        if (sendSize <= 0)
-        {
-            HandleError("sendto return <= 0");
-            return 0;
-        }
-    }
-
+    ::closesocket(servSock);
     ::WSACleanup();
     return 0;
 }
