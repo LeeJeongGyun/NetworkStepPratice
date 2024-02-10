cd8a439eeae9d4b07be4fbdc2a1b6923278a6f21
JKLEE <jeonggyun0630@gmail.com>
2024-02-10 22:25:45 +0900

    WSAEventSelect Simple Pratice

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -66,99 +66,114 @@ int main()
 
     std::cout << "Accept" << std::endl;
     
-    // select 모델
-    // 리눅스, 윈도우 호환
-    // 64개 제한.
-    // 소켓 함수 호출 성공 시점 미리 알 수 있음.
-    // 문제 상황)
-    // 수신 버퍼 데이터 없는데 read
-    // 송신 버퍼가 꽉 찼는데, write
-    // - 블로킷 소켓: 조건이 만족되지 않아 블로킹 되는 상황 예방
-    // - 논블로킹 소켓: 조건이 만족되지 않아 불필요하게 반복 체크 예방ㄴ
+    // WSAEventSelect
+    // 64개 제한
 
-    // FD_ZERO : 초기화
-    // FD_SET  : 소켓 설정
-    // FD_CLR  : 소켓 삭제
-    // FD_ISSET : 소켓 확인
+    // 생성 : WSACreateEvent( 수동 리셋(Manual Reset), Non-Signaled )
+    // 삭제 : WSACloseEvent
+    // 신호 상태 감지 : WSAWaitForMutipleEvents
+    // 구체적인 네트워크 이벤트 확인 : WSAEnumNetworkEvents
 
+    // 소켓 <-> 이벤트 객체 연동 필요
+    // FD_ACCEPT, FD_READ, FD_WRITE, FD_CLOSE, FD_CONNECT
+
+    // 주의
+    // WSAEventSelect 함수 호출 시 해당 소켓 자동으로 넌블로킹 전환
+    // accept() 함수 리턴하는 소켓은 listenSock과 동일한 속성
+    // 따라서, 리턴 소켓 FD_READ, RD_WRITE 등록  필요
+    // WSAEWOULDBLOCK 예외 처리 필요
+    // 이벤트 발생 시 적절한 함수 호출 필요 -> 그렇지 않다면 네트워크 이벤트 발생하지 않음
+    // ex) FD_READ 발생히 recv 함수 무조건 호출 필요
+
+    // WSAEnumNetworkEvents
+    // 인자로 들어온 event 객체를 non-signaled 상태로 변경
+
+    std::vector<WSAEVENT> events;
     std::vector<Session> sessions;
-    sessions.reserve(100);
 
-    fd_set readSet, writeSet;
+    WSAEVENT listenEvent = ::WSACreateEvent();
+    events.push_back(listenEvent);
+    sessions.push_back(Session{ listenSock });
+    if (SOCKET_ERROR == ::WSAEventSelect(listenSock, listenEvent, FD_ACCEPT | FD_CLOSE))
+    {
+        HandleError("::WSAEventSelect error");
+        return 0;
+    }
+
     while (true)
     {
-        FD_ZERO(&readSet);
-        FD_ZERO(&writeSet);
+        int retIdx = ::WSAWaitForMultipleEvents(static_cast<DWORD>(events.size()), &events[0], FALSE, INFINITE, FALSE);
+        if (retIdx == WSA_WAIT_FAILED) continue;
 
-        FD_SET(listenSock, &readSet);
-        for (Session& s : sessions)
-        {
-            if (s.recvBytes <= s.sendBytes) FD_SET(s.sock, &readSet);
-            else FD_SET(s.sock, &writeSet); 
-        }
+        retIdx -= WSA_WAIT_EVENT_0;
+
+        WSANETWORKEVENTS netEvents;
+        // 자동으로 non-signaled로 변경해줌
+        if (SOCKET_ERROR == ::WSAEnumNetworkEvents(sessions[retIdx].sock, events[retIdx], &netEvents)) continue;
         
-        // 적어도 하나의 소켓이 준비되면 리턴 -> 준비되지 않은 소켓들 삭제.
-        int retVal = ::select(0, &readSet, &writeSet, nullptr, nullptr);
-        if (retVal == SOCKET_ERROR)
+        if (netEvents.lNetworkEvents & FD_ACCEPT)
         {
-            HandleError("::select error");
-            break;
-        }
+            if (netEvents.iErrorCode[FD_ACCEPT_BIT] != 0) continue;
 
-        if (FD_ISSET(listenSock, &readSet))
-        {
             SOCKADDR_IN clntAdr;
             int clntAdrSize = sizeof(clntAdr);
-            SOCKET clntSock = ::accept(listenSock, reinterpret_cast<SOCKADDR*>(&clntAdr), &clntAdrSize);
-            if (clntSock == INVALID_SOCKET)
-            {
-                HandleError("::accept error");
-                return 0;
-            }
-            else
-            {
-                sessions.push_back(Session{clntSock});
-            }
 
-            std::cout << "Accept Success!!" << std::endl;
+            SOCKET clntSock = ::accept(listenSock, reinterpret_cast<SOCKADDR*>(&clntAdr), &clntAdrSize);
+            if (clntSock != INVALID_SOCKET)
+            {
+                std::cout << "Client Success" << std::endl;
+
+                WSAEVENT clntEvent = ::WSACreateEvent();
+                events.push_back(clntEvent);
+                sessions.push_back(Session{ clntSock });
+
+                if (SOCKET_ERROR == ::WSAEventSelect(clntSock, clntEvent, FD_READ | FD_WRITE | FD_CLOSE)) continue;
+            }
         }
 
-        for (Session& s : sessions)
+        if (netEvents.lNetworkEvents & FD_READ || netEvents.lNetworkEvents & FD_WRITE)
         {
-            if (FD_ISSET(s.sock, &readSet))
+            if ((netEvents.lNetworkEvents & FD_READ) && (netEvents.iErrorCode[FD_READ_BIT] != 0)) continue;
+
+            if ((netEvents.lNetworkEvents & FD_WRITE) && (netEvents.iErrorCode[FD_WRITE_BIT] != 0)) continue;
+
+            Session s = sessions[retIdx];
+            if (s.recvBytes <= s.sendBytes)
             {
                 int recvSize = ::recv(s.sock, s.recvBuffer, BUFF_SIZE, 0);
-                if (recvSize <= 0)
+                if (recvSize == SOCKET_ERROR && ::WSAGetLastError() != WSAEWOULDBLOCK)
                 {
-                    // 제거 필요
+                    // 에러 처리 필요
                     continue;
                 }
 
-                s.recvBytes = recvSize;
+                s.recvBytes     = recvSize;
                 std::cout << "Recv Size: " << recvSize << std::endl;
                 std::cout << "Recv Data: " << s.recvBuffer << std::endl;
             }
             
-            if (FD_ISSET(s.sock, &writeSet))
+            if(s.recvBytes > s.sendBytes)
             {
-                // 블로킹 모드 -> 모든 데이터 보낼 때 까지 대기.
-                // 논블로킹 모드 -> 일부만 보낼 경우 존재.
-                int sendSize = ::send(s.sock, &s.recvBuffer[s.sendBytes], s.recvBytes - s.sendBytes, 0);
-                if (sendSize <= 0)
+                int sendSize = ::send(s.sock, &s.recvBuffer[s.recvBytes - s.sendBytes], s.recvBytes - s.sendBytes, 0);
+                if (sendSize == SOCKET_ERROR && ::WSAGetLastError() != WSAEWOULDBLOCK)
                 {
-                    // 제거 필요
+                    // 에러 처리 필요
                     continue;
                 }
-
+                
+                std::cout << "Send Success!!" << std::endl;
                 s.sendBytes += sendSize;
-                std::cout << "Send Size: " << sendSize << std::endl;
-
-                if (s.sendBytes == s.recvBytes)
+                if (s.recvBytes == s.sendBytes)
                 {
                     s.recvBytes = 0;
                     s.sendBytes = 0;
                 }
             }
+        }
+        
+        if (netEvents.lNetworkEvents & FD_CLOSE)
+        {
+            // 삭제 필요
         }
     }
 
